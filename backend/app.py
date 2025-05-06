92d56b00740a6de1c56872c98cde18bc0b59b0ea
rockmanzerogx <batsujori@gmail.com>
2025-05-06 18:38:28 -0400

    Agrega cors

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app)
 API_KEY = os.getenv("DEEPSEEK_API_KEY")  # Render inyectará esta variable
 API_URL = "https://api.deepseek.com/v1/chat/completions"
 
